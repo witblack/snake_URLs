d5a988153f2777fbc1bdc66bf74a54e0b74bb7ba
WitBlack <60584448+witblack@users.noreply.github.com>
2020-04-17 22:08:18 +0430

    Update Portable.py

--- Portable.py
+++ Portable.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python
 # coding: latin-1
-
-#Import Libs From Python
+# Writed By WitBlack HAcker
+# Web ~> Https://0man.ir
+# Email ~> admin@0man.ir
 from googlesearch import search
 from os import system
 from os.path import exists
-#from termcolor import colored
-#from rhinoscriptsyntax import CreateColor as cc
-
-#Setting Colors
 bblue = "\\[\\033[0;38;5;12m\\]"
 black = "\\[\\033[0;38;5;0m\\]"
 blue = "\\[\\033[0;38;5;4m\\]"
@@ -25,8 +22,6 @@ smoothgreen = "\\[\\033[0;38;5;42m\\]"
 turqoise = "\\[\\033[0;38;5;50m\\]"
 white = "\\[\\033[0;38;5;6m\\]"
 yellow = "\\[\\033[0;38;5;3m\\]"
-
-#Functions
 def Clear():
 	if OS == 'Windows':
 		system('cls')
@@ -55,38 +50,24 @@ def Banner():
 	print('      \/_______/   \/_/   \/________/   \/__________/     \/_/   \/_/')
 	print("\nProgramed By WitBlack HAcker. - Web ~> Https://0man.ir")
 	print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-
-#Checking Os Type
 if exists('C:/Windows/'):
 	OS = 'Windows'
 elif exists('/etc/'):
 	OS = 'Linux'
 else:
 	exit('Sorry! Your OS Not Support. :(')
-
-#Clear The Page And Showing Banner
 Banner()
-
-#Send A Tip For Users
 system('wget https://0man.ir/Server/Snake_URL_TIP.php -O /tmp/SnakeURLs_TIP 1> /dev/null 2>&1')
-
-#Geting Help Or Google Search Options
 print("\nEnter 'h' For Get Help. 'q' for exit.")
 Query = raw_input("Please Enter Query ~> ")
-
-#Checking Input. If User Enter 'h' Showing Help And Exit.
-#Also Check 'q' For Exit
 if str.lower(Query) == "h" or str.lower(Query) == 'help':
 	print('For See More Information Read:')
 	exit('http://www.googleguide.com/advanced_operators_reference.html')
 elif str.lower(Query) == 'q' or str.lower(Query) == 'exit' or str.lower(Query) == 'quit':
 	exit("\n\nQuiting...")
-
-#Saving Export File Option
 WriteType = ''
 SaveInFile = raw_input('Do You Want Save In A File? [y/n] ')
 if str.lower(SaveInFile) == 'y' or str.lower(SaveInFile) == 'yes' or str.lower(SaveInFile) == 'ok':
-	#If It's Ture Ask File Name Or File Address. If File Exist Ask Ower Write? Append Or Write?
 	while WriteType == '':
 		SaveInFile = raw_input('Enter Name Or File Address: ')
 		if exists(SaveInFile):
@@ -101,31 +82,22 @@ if str.lower(SaveInFile) == 'y' or str.lower(SaveInFile) == 'yes' or str.lower(S
 				exit("\nCancelled Search!")
 		else:
 			WriteType = 'Write'
-
-#Showing Resault Report Type
 if WriteType == '':
 	print("Searching And Showing Result(s) ...\n\n")
 elif str.lower(WriteType) == 'w' or str.lower(WriteType) == 'write':
 	print('Searching And Write Result(s) At "' + SaveInFile + '"')
 else:
 	print('Searching And Append Result(s) At "' + SaveInFile + '"')
-
-#Opeing File For Write Or Append If Need Write
 if WriteType != '':
 	if str.lower(WriteType) == 'w' or str.lower(WriteType) == 'write':
 		File = open(SaveInFile ,'w')
 	else:
 		File = open(SaveInFile ,'a')
-
-#Gettind Resualt(s) One By One. Show Or Write Or Appned Them
 for Resualt in search(Query):
-	#, tld="co.in", num=10, stop=1, pause=2):
 	if WriteType != '':
 		File.write("\n" + Resualt)
 	else:
 		print(Resualt)
-
-#Close Opened File And Exit
 if WriteType != '':
 	File.close()
 print("\n\nQuiting...")
