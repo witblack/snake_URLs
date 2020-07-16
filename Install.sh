b19c20d9a90e728eef8fcda31ec193091f8695bd
WitBlack <60584448+witblack@users.noreply.github.com>
2020-07-16 16:25:39 +0430

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 if [[ $EUID -ne 0 ]]; then
-   echo "Please Run Script As Root User." 
+   echo "Please Run Script As Root User." ;
    exit 1;
 fi
-COD=0
+COD=0;
 mkdir /tmp/Snake_URLs_Installer 1> /dev/null 2>&1;
 cd /tmp/Snake_URLs_Installer;
 touch ERRORS;
@@ -11,12 +11,15 @@ git clone https://github.com/witblack/snake_URLs.git 2> /dev/null 1> ERRORS;
 ERRORS=$(cat ERRORS);
 if [$ERRORS == ''];
 then
-	mv snake_URLs/Portable.py /usr/bin/snake
-	chmod 555 /usr/bin/snake
+	apt-get install pip -y;
+	pip install googlesearch;
+	pip install os;
+	mv snake_URLs/Portable.py /usr/bin/snake;
+	chmod 555 /usr/bin/snake;
 	echo 'Installed Successfully'
 else
 	echo 'Error Connect To GitHub.com! Check "git" Package Be Installed.';
-	COD=1
+	COD=1;
 fi
-rm -r /tmp/Snake_URLs_Installer; 1> /dev/null 2>&1
+rm -r /tmp/Snake_URLs_Installer 1> /dev/null 2>&1;
 exit $COD;
