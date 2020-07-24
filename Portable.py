3154c37d37a8558fc1a4d12d8961bf8ea17b95b8
WitBlack <60584448+witblack@users.noreply.github.com>
2020-07-24 19:55:25 +0430

    Update Portable.py

--- Portable.py
+++ Portable.py
@@ -59,10 +59,14 @@
 #📧 E-Mail:
 #admin@bugzone.ir
 #
-from googlesearch import search
-from os import system
-from os import name
-from os.path import exists
+try:
+	from googlesearch import search
+	from os import system
+	from os import name
+	from os.path import exists
+except:
+	print('Some libs not installed! Pls install all "requires.txt".')
+	exit(1)
 def Clear():
 	if name == 'nt':
 		system('cls')
@@ -90,7 +94,7 @@ def Banner():
 	print('      \/_______/   \/_/   \/________/   \/__________/     \/_/   \/_/')
 	print("\nProgramed By WitBlack HAcker. - Web ~> Https://BugZone.ir")
 	print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-	print('VERSION 1.0.2')
+	print('VERSION 1.0.3')
 if not (exists('C:/Windows/') or exists('/etc/')):
 	exit('Sorry! Your OS Not Support. :(')
 Banner()
@@ -98,46 +102,50 @@ Banner()
 #    os.system('wget https://BugZone.ir/Server/SnakeTIP.php -O /tmp/SnakeURLs_TIP 1> /dev/null 2>&1')
 #except Exception as GettingError:
 #    print('Cannot send a using TIP! Check your connection\n Error: {}', .format(GettingError))
-print("\nEnter 'h' For Get Help. 'q' for exit.")
-Query = raw_input("Please Enter Query ~> ")
-if str.lower(Query) == "h" or str.lower(Query) == 'help':
-	print('For See More Information Read:')
-	exit('http://www.googleguide.com/advanced_operators_reference.html')
-elif str.lower(Query) == 'q' or str.lower(Query) == 'exit' or str.lower(Query) == 'quit':
-	exit("\n\nQuiting...")
-WriteType = ''
-SaveInFile = raw_input('Do You Want Save In A File? [y/n] ')
-if str.lower(SaveInFile) == 'y' or str.lower(SaveInFile) == 'yes' or str.lower(SaveInFile) == 'ok':
-	while WriteType == '':
-		SaveInFile = raw_input('Enter Name Or File Address: ')
-		if exists(SaveInFile):
-			WriteType = raw_input('File Or Address Exist. Continue? [y/n] ')
-			if str.lower(WriteType) == 'yes' or str.lower(WriteType) == 'y' or str.lower(WriteType) == 'ok':
-				WriteType = raw_input('Append or Write? [a/w] ')
-				if str.lower(WriteType) == 'w' or str.lower(WriteType) == 'write':
-					WriteType = 'Write'
+try:
+	print("\nEnter 'h' For Get Help. 'q' for exit.")
+	Query = raw_input("Please Enter Query ~> ")
+	if str.lower(Query) == "h" or str.lower(Query) == 'help':
+		print('For See More Information Read:')
+		exit('http://www.googleguide.com/advanced_operators_reference.html')
+	elif str.lower(Query) == 'q' or str.lower(Query) == 'exit' or str.lower(Query) == 'quit':
+		exit("\n\nQuiting...")
+	WriteType = ''
+	SaveInFile = raw_input('Do You Want Save In A File? [y/n] ')
+	if str.lower(SaveInFile) == 'y' or str.lower(SaveInFile) == 'yes' or str.lower(SaveInFile) == 'ok':
+		while WriteType == '':
+			SaveInFile = raw_input('Enter Name Or File Address: ')
+			if exists(SaveInFile):
+				WriteType = raw_input('File Or Address Exist. Continue? [y/n] ')
+				if str.lower(WriteType) == 'yes' or str.lower(WriteType) == 'y' or str.lower(WriteType) == 'ok':
+					WriteType = raw_input('Append or Write? [a/w] ')
+					if str.lower(WriteType) == 'w' or str.lower(WriteType) == 'write':
+						WriteType = 'Write'
+					else:
+						WriteType = 'Append'
 				else:
-					WriteType = 'Append'
+					exit("\nCancelled Search!")
 			else:
-				exit("\nCancelled Search!")
-		else:
-			WriteType = 'Write'
-if WriteType == '':
-	print("Searching And Showing Result(s) ...\n\n")
-elif str.lower(WriteType) == 'w' or str.lower(WriteType) == 'write':
-	print('Searching And Write Result(s) At "' + SaveInFile + '"')
-else:
-	print('Searching And Append Result(s) At "' + SaveInFile + '"')
-if WriteType != '':
-	if str.lower(WriteType) == 'w' or str.lower(WriteType) == 'write':
-		File = open(SaveInFile ,'w')
+				WriteType = 'Write'
+	if WriteType == '':
+		print("Searching And Showing Result(s) ...\n\n")
+	elif str.lower(WriteType) == 'w' or str.lower(WriteType) == 'write':
+		print('Searching And Write Result(s) At "' + SaveInFile + '"')
 	else:
-		File = open(SaveInFile ,'a')
-for Result in search(Query):
+		print('Searching And Append Result(s) At "' + SaveInFile + '"')
 	if WriteType != '':
-		File.write("\n" + Result)
-	else:
-		print(Result)
+		if str.lower(WriteType) == 'w' or str.lower(WriteType) == 'write':
+			File = open(SaveInFile ,'w')
+		else:
+			File = open(SaveInFile ,'a')
+	for Result in search(Query):
+		if WriteType != '':
+			File.write("\n" + Result)
+		else:
+			print(Result)
+except:
+        print("Exit.\n")
+        exit(1)
 if WriteType != '':
 	File.close()
 print("\n\nQuiting...")
