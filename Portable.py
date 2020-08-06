6bae73d7bcc17f7e0beeb76831d3d47569955a08
WitBlack <60584448+witblack@users.noreply.github.com>
2020-08-06 22:12:51 +0430

    Update Portable.py

--- Portable.py
+++ Portable.py
@@ -38,27 +38,7 @@
 #
 #📧 E-Mail:
 #admin@bugzone.ir
-#Powered By WitBlack Hacker
-#Version 1.0.2 - Meli Code Generator
-#
-#💬 Telegram:
-#Https://t.me/WitBlack_ch
-#
-#💻 Web:
-#Https://BugZone.ir
-#
-#📹 YouTube:
-#https://www.youtube.com/channel/UCIgk2ldVeelyaHW3s4UkIIg (WitBlack)
-#
-#🎥 Aparat:
-#Https://aparat.com/WitBlack
-#
-#⌨️ Github:
-#Https://github.com/WitBlack
-#
-#📧 E-Mail:
-#admin@bugzone.ir
-#
+
 try:
 	from googlesearch import search
 	from os import system
