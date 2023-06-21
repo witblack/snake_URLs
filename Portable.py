51724a0fa006440d7b8a057f0a5c4d82f3240f3a
WitBlack <60584448+witblack@users.noreply.github.com>
2023-06-21 09:58:33 +0330

    Update Portable.py

--- Portable.py
+++ Portable.py
@@ -3,41 +3,6 @@
 #---------------------------
 # Writed By WitBlack HAcker
 #---------------------------
-#💬 Telegram:
-#Https://t.me/WitBlack_ch
-#
-#💻 Web:
-#Https://BugZone.ir
-#
-#📹 YouTube:
-#https://www.youtube.com/channel/UCIgk2ldVeelyaHW3s4UkIIg (WitBlack)
-#
-#🎥 Aparat:
-#Https://aparat.com/WitBlack
-#
-#⌨️ Github:
-#Https://github.com/WitBlack
-#
-#📧 E-Mail:
-#admin@bugzone.ir
-#
-#💬 Telegram:
-#Https://t.me/WitBlack_ch
-#
-#💻 Web:
-#Https://BugZone.ir
-#
-#📹 YouTube:
-#https://www.youtube.com/channel/UCIgk2ldVeelyaHW3s4UkIIg (WitBlack)
-#
-#🎥 Aparat:
-#Https://aparat.com/WitBlack
-#
-#⌨️ Github:
-#Https://github.com/WitBlack
-#
-#📧 E-Mail:
-#admin@bugzone.ir
 
 try:
 	from googlesearch import search
